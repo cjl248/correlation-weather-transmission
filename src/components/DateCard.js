693eba134d751e0185a6d98ed252b4d7d25a1c2e
github cjl248 <github cjl248@cornell.edu>
2020-05-01 12:44:23 -0400

    clicking on a card now scrolls to the top to show its details

--- src/components/DateCard.js
+++ src/components/DateCard.js
@@ -2,16 +2,43 @@ import React from 'react'
 
 export default class DateCard extends React.Component {
 
+
   formatDate = () => {
     const fullDate = this.props.date
     const dateArray = fullDate.split("T")
     return dateArray[0]
   }
 
+  toTop = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
+
+
+  // NEED TO PULL CHANGES FROM BRANCH THAT HAD FIXED THE NEW/TOTAL CASE/DEATH
+  // COUNT. ONLY MERGED INTO MASTER BUT NOT INTO THIS BRANCH. NEED TO MERGE
+  // CHANGES IN 'DateCardContainer' INTO THIS BRANCH SO THAT DATA IN PROPS GETS
+  // PASSED TO 'DateCard'. THEN 'handleClick' CAN USE 'setActiveDateCard' AND
+  // 'toTop' AS EXPECTED
+  handleClick = () => {
+    const {
+      newCases,
+      totalCases,
+      totalDeaths,
+      newDeaths,
+      setActiveDateCard
+    } = this.props
+    setActiveDateCard(this.formatDate(), newCases, newDeaths, totalCases, totalDeaths)
+    this.toTop()
+  }
+
   render() {
-    const {newCases, totalCases, totalDeaths, newDeaths, setActiveDateCard } = this.props
+    const { newCases, totalCases, totalDeaths, newDeaths } = this.props
     return (
-      <div className={"date-card"} onClick={() => {setActiveDateCard(this.formatDate(), newCases, newDeaths, totalCases, totalDeaths)}}>
+      <div className={"date-card"} onClick={this.handleClick}>
         <h2>{this.formatDate()}</h2>
         <p>{`New Cases: ${newCases}`}</p>
         <p>{`New Deaths: ${newDeaths}`}</p>
